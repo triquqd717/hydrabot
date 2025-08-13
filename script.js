4b08ae14578f4518c12237834739966bf4f9c68b
triquqd717 <actlikeuh@gmail.com>
2025-08-13 21:13:59 +0000

    hi

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+    }
+
+    themeToggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        let theme = 'light-mode';
+        if (document.body.classList.contains('dark-mode')) {
+            theme = 'dark-mode';
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
